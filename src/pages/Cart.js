c5499cd9a2ad9d2e90b26b62fb5c6f7cdd3b4276
sunilpanakal <sunilpanakal@gmail.com>
2024-04-11 11:49:28 +0530

    second commit

---
+++ src/pages/Cart.js
@@ -0,0 +1,21 @@
+
+import { CartCard } from "../components"
+import { useTitle } from "../hooks/useTitle"
+import { useCart } from "../context/CartContext";
+
+export const Cart = () => {
+  const { total, cartList } = useCart();
+  useTitle("Cart");
+
+  return (
+    <main>
+      <section className="cart">
+        <h1>Cart Items: {cartList.length} / ${total}</h1>
+        {cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+
+      </section>
+    </main>
+  )
+}
